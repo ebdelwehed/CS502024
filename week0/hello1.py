964dab967c3a80c67e7e2f93f041080c55d1702e
ebdelweheh <abdulwahid1@gmail.com>
2025-01-08 16:33:35 +0300

    introduction to comments

--- week0/hello1.py
+++ week0/hello1.py
@@ -1,2 +1,8 @@
-name = input("what is your name")
-print("hello",name)
+#name = input("what is your name")
+#print("hello",name)
+#create variable name
+name = input("what's your name ?")
+#use print function to display hello
+print("hello ")
+#display variable name
+print(name)
