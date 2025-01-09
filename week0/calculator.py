42aec08d6b0828d497ffca47f0c3fe08cae75ff2
ebdelweheh <abdulwahid1@gmail.com>
2025-01-09 14:49:57 +0300

    introduction to defining functions

--- week0/calculator.py
+++ week0/calculator.py
@@ -6,10 +6,19 @@ z = x + y
 print(z)
 
 #ask user for what's x ?
-x = input("what's x ?")
+# x = input("what's x ?")
+# #ask user for what's y ?
+# y = input("what's y ?")
+# #z holds addition value of x and y
+# z = int(x) + int(y)
+# #display value of z
+# print(z)
+
+#ask user for what's x ?
+x = int(input("what's x ?"))
 #ask user for what's y ?
-y = input("what's y ?")
-#z holds addition value of x and y
-z = int(x) + int(y)
-#display value of z
-print(z)
+y = int(input("what's y ?"))
+
+
+#display value 
+print(x + y)
