abf8631ac840426022fe1a59c1e163f1de54414f
ebdelweheh <abdulwahid1@gmail.com>
2025-01-09 10:25:04 +0300

    added calculator app

---
+++ week0/calculator.py
@@ -0,0 +1,15 @@
+x = 1
+y = 2
+
+z = x + y
+
+print(z)
+
+#ask user for what's x ?
+x = input("what's x ?")
+#ask user for what's y ?
+y = input("what's y ?")
+#z holds addition value of x and y
+z = int(x) + int(y)
+#display value of z
+print(z)
