f438445f75454b40da3ea4ceb7338f3864d703d5
ebdelweheh <abdulwahid1@gmail.com>
2025-01-17 20:34:19 +0300

    minor changes

---
+++ week0/calculator6.py
@@ -0,0 +1,7 @@
+# Demonstrates fewer variables and more functions
+
+x = float(input("what's x ? "))
+y = float(input("what's y ? "))
+z = round(x 
+          +y)
+print("The sum of x and y is " , z)
